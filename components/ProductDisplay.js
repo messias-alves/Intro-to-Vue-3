8f5d72174d201d5a3bb97e7a4c02f65a04541b10
Digital-Meta-Group <messias.apneto@gmail.com>
2022-11-09 01:22:33 -0300

    finish-course

---
+++ components/ProductDisplay.js
@@ -0,0 +1,85 @@
+app.component('product-display', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    `<div class="product-display">
+        <div class="product-container">
+            <div class="product-image">
+                <img v-bind:src="image">
+            </div>
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p v-if="inventory > 10">In Stock</p>
+                <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+                <p v-else>Out of stock</p>
+
+                <p>Shipping: {{ shipping }}</p>
+                <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+                <div 
+                v-for="(variant, index) in variants" 
+                :key="variant.id" 
+                v-on:mouseover="updateVariant(index)"
+                class="color-circle"
+                :style="{ backgroundColor: variant.color }">
+            </div>
+                <button 
+                class="button" 
+                :class="{ disabledButton: inventory == 0}"
+                :disabled="inventory == 0"
+                v-on:click="addToCart">
+                Add to Cart
+            </button>
+            </div>
+        </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
+    </div>`,
+    data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
+            ],
+            reviews: []
+        }
+    },
+    methods: {
+        addToCart() {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        },
+        addReview(review) {
+            this.reviews.push(review)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+           return this.variants[this.selectedVariant].image
+        },
+        inventory() {
+            return this.variants[this.selectedVariant].quantity
+         },
+         shipping() {
+            if (this.premium) {
+                return 'Free'
+            }
+            return 2.99
+         }
+    }
+})
